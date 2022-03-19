70ce1ae42d50229e2d8208c451cfa52078852a4a
tifuseini <tifuseini@st.ug.edu.gh>
2022-03-19 10:03:38 +0000

    ListMaker Initail Commit

---
+++ Listmaker/app/src/main/java/com/tifuseini/listmaker/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.tifuseini.listmaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tifuseini.listmaker.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
